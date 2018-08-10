c614aad15f46f867fb008aac7d2cede9da6db581
Fuzoka <unbeatablesites09@gmail.com>
2018-08-09 23:59:28 -0400

    add

--- server.js
+++ server.js
@@ -1,25 +1,72 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+let waitingList = [];
+
+let tables = [
+ {
+   customerName: "frank",
+   phoneNumber: "123-456-7890",
+   customerEmail: "frank@gmail.com",
+   customerID: 900
+ },
+ {
+   customerName: "Zarr",
+   phoneNumber: "234-567-8901",
+   customerEmail: "zarr@gmail.com",
+   customerID: 1900
+ },
+ {
+   customerName: "may",
+   phoneNumber: "345-678-9012",
+   customerEmail: "may@gmail.com",
+   customerID: 800
+ }
+];
 
 
-
-
-
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, "view.html"));
 });
 
+app.get("/table", (request, response) => {
+    response.sendFile(path.join(__dirname, "table.html"));
+});
+
+app.get("/reserve", (request, response) => {
+    response.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/api/tables", (request, response) => {
+    return response.json(tables);
+});
+
+app.get("/api/waitlist", (request, response) => {
+    return response.json(waitingList);
+});
+
+app.post("/api/tables", (request, response) => {
+    let newTable = request.body;
+    
+    if (tables.length >= 5) {
+        waitingList.push(newTable);
+    }
+    else {
+        tables.push(newTable);
+    }
+
+    response.sendFile(path.join(__dirname, "add.html"));
+});
+
+
+
+app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+});
